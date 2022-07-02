1514319c7c2337773ba3da4d008b181c3dc0a90b
Sophoca <tjdqls1668@naver.com>
2022-07-03 00:46:32 +0900

    initial setting

---
+++ src/main/java/kr/ac/ajou/studyplanner/StudyplannerApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.ajou.studyplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyplannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/ajou/studyplanner/StudyplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.ajou.studyplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
